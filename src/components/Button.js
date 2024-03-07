68e5bf0f701e8d26d4d65ee319c293ba0c6dcd50
teruboi <reynaldinickolas@gmail.com>
2024-03-08 02:59:01 +0700

    Added visi misi card

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function ButtonDefault({ text, url, variant }) {
-    let button = 'h-fit w-fit px-10 py-2 text-[34px] text-white rounded-[32px] transition-all'
+    let button = 'h-fit w-fit px-10 py-1 text-[34px] text-white rounded-[32px] transition-all mx-auto my-5'
     if (variant !== null){
         switch (variant) {
             case 1:
@@ -11,7 +11,7 @@ export default function ButtonDefault({ text, url, variant }) {
                 button += ' bg-secondary h-fit w-fit px-10 py-2 text-[34px] text-white rounded-[32px]'
                 break;
             case 3:
-                button += ' bg-white bg-opacity-50 border-[3px] border-white hover:bg-secondary hover:bg-opacity-50 hover:border-secondary h-fit w-fit px-10 py-2 text-[34px] text-white rounded-[32px]'
+                button += ' bg-white bg-opacity-50 border-[3px] border-white group-hover:bg-secondary group-hover:bg-opacity-50 group-hover:border-secondary'
                 break;
             default:
                 break;
