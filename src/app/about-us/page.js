68df3195c58ea7fcee155d0f67177b5ba781560a
teruboi <reynaldinickolas@gmail.com>
2024-03-20 23:15:50 +0700

    update

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function AboutUs() {
     return(
         <>
-        <div className="w-fit h-fit bg-gradient-to-t from-white from-10% to-40%">
-            <div style={{backgroundImage:'url(about-us-hero.png)'}} className="w-screen h-screen bg-gradient-to-t from-white from-10% to-40%">
-                <div className="text-[140px] font-bold text-secondary text-center w-full h-full align-text-bottom">
-                    <h1 className="">About Us</h1>
-                </div>
+        <div className="max-w-[1920px] max-h-[1080px] w-screen h-screen bg-gradient-to-t from-white from-10% to-40%">
+            <div
+            /*style={{backgroundImage:'url(about-us-hero.png)', backgroundRepeat:'no-repeat'}}*/
+            className="w-full h-full bg-secondary">
+                <div className="relative text-secondary font-bold text-[140px] w-fit h-fit">About Us</div>
             </div>
         </div>
         </>
